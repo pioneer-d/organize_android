afb859601dca6bfb085314ae025aae32c6a29b49
pioneer-d <pioneer-d@naver.com>
2022-06-28 00:16:32 +0900

    Activity_Lifecycle_1
    Activity 생명주기 정리

--- app/src/main/java/com/kotlin/basic/MainActivity.kt
+++ app/src/main/java/com/kotlin/basic/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.kotlin.basic
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kotlin.basic.lifecycle_1.Lifecycle_activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent = Intent(this, Lifecycle_activity::class.java)
+        startActivity(intent)
+
     }
+
+
 }
---
+++ app/src/main/java/com/kotlin/basic/lifecycle_1/Lifecycle_activity.kt
@@ -0,0 +1,53 @@
+package com.kotlin.basic.lifecycle_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.kotlin.basic.R
+
+class Lifecycle_activity : AppCompatActivity() {
+
+    val thisName = "Lifecycle_activity"
+
+    // step_1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lifecycle)
+    }
+
+    // step_2
+    override fun onStart() {
+        super.onStart()
+        Log.d(thisName,"onStart()")
+    }
+
+    // step_3
+    override fun onResume() {
+        super.onResume()
+        Log.d(thisName,"onResume()")
+    }
+
+    // step_4
+    override fun onPause() {
+        super.onPause()
+        Log.d(thisName,"onPause()")
+    }
+
+    // step_5
+    override fun onStop() {
+        super.onStop()
+        Log.d(thisName,"onStop()")
+    }
+
+    // step_5
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(thisName,"onRestart()")
+    }
+
+    // step_6
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(thisName,"onDestroy()")
+    }
+}
